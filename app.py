36984f663ce13affb16272b24fac871115fbf8e4
wyoung163 <choiwy0906@gmail.com>
2023-12-04 17:39:10 +0900

    fix: add given name

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ def signUp():
     req = request.json
     user_name = req['user_name']
     user_sn = req['user_sn']
-    #user_gname = req['user_gname']
+    user_gname = req['user_gname']
     user_mail = req['user_mail']
     user_passwd = req['user_passwd']
     
-    res = signup.add_user(user_name=user_name, user_sn=user_sn, user_mail=user_mail, user_passwd=user_passwd)
-    if res==true:
+    res = signup.add_user(user_name=user_name, user_sn=user_sn, user_gname=user_gname, user_mail=user_mail, user_passwd=user_passwd)
+    if res == True:
         return "success"
     else:
         return "fail"
