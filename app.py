0fe54460d9e8fc88f39523c3479ea1ee98aefad7
wyoung163 <choiwy0906@gmail.com>
2023-12-04 11:04:27 +0900

    feat: add signup post api

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
-#@app.route('/')
-#def hello_world():
-#    return 'Hello World!'
+#import sys, os
+#sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from controllers import signup
+
+@app.route('/signup', methods=['POST'])
+def signUp():
+    req = request.json
+    user_name = req['user_name']
+    user_sn = req['user_sn']
+    #user_gname = req['user_gname']
+    user_mail = req['user_mail']
+    user_passwd = req['user_passwd']
+    
+    res = signup.add_user(user_name=user_name, user_sn=user_sn, user_mail=user_mail, user_passwd=user_passwd)
+    return res
 
 if __name__ == '__main__':
     app.run(port='3000')
