93e6e8e5d070fc477aa967a0b4067f510bda5fc9
wyoung163 <choiwy0906@gmail.com>
2023-12-26 11:38:08 +0900

    chore: swift connection 추가

--- controllers/ks_auth.py
+++ controllers/ks_auth.py
@@ -2,8 +2,10 @@
 from keystoneauth1.identity import v3
 from keystoneauth1 import session
 from keystoneclient.v3 import client
+from swiftclient import Connection
 import config
-# Keystone server 연결
+
+# Keystone Server 연결
 auth = v3.Password(auth_url=config.KS_URL,
         username=config.KS_ADMIN_USERNAME,
         password=config.KS_ADMIN_PASSWORD,
@@ -11,3 +13,6 @@ auth = v3.Password(auth_url=config.KS_URL,
         user_domain_id=config.KS_USER_DOMAIN_ID)
 sess = session.Session(auth=auth)
 keystone = client.Client(session=sess)
+
+# Swift Client 연결
+swift = Connection(session=sess)
