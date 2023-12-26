c1525f436d43f901fe7827245e3f43aa76cb1ac9
wyoung163 <choiwy0906@gmail.com>
2023-12-26 17:59:41 +0900

    docs: config attribute 명칭 변경

--- app.py
+++ app.py
@@ -11,18 +11,37 @@ import os
 
 app = Flask(__name__)
 api = Api(app, version='1.0', title='인증 API', description='Swagger', doc="/api-docs")
-auth_api = api.namespace('', description='SignUp API')
+auth_api = api.namespace('auth', description='회원가입/탈퇴 API')
+group_api = api.namespace('group', description='사용자 초대 API')
 
 class _Schema():
-    post_fields = auth_api.model("회원 가입 시 필요한 데이터", {
-        # 'username': fields.String(description="사용자 아이디", example="cwy"),
-        # 'last_name': fields.String(description="사용자 성", example="choi"),
-        # 'given_name': fields.String(description="사용자 이름", example="wyoung"),
-        # 'email': fields.String(description="사용자 이메일", example="cwy@test.com"),
-        # 'password': fields.String(description="사용자 비밀번호", example="1234"),
+    post_fields = auth_api.model("[개인 회원] 회원 가입 시 필요한 데이터", {
+        'id': fields.String(description="아이디", example="cwy"),
+        'username': fields.String(description="사용자 이름", example="최누구"),
+        'email': fields.String(description="이메일", example="cwy@test.com"),
+        'password': fields.String(description="비밀번호", example="1234"),
+        'phone': fields.String(description="전화번호", example="010-0000-0000"),
+        'address': fields.String(description="주소", example="서울특별시 성북구 화랑도 11길 26"),
     })
 
-@auth_api.route('/auth')
+    post_fields_2 = auth_api.model("[기업 회원] 회원 가입 시 필요한 데이터", {
+        'id': fields.String(description="아이디", example="company01"),
+        'name': fields.String(description="담당자 이름", example="최누구"),
+        'company': fields.String(description="회사 이름", example="company01"),
+        'email': fields.String(description="이메일", example="test@company.co.kr"),
+        'password': fields.String(description="비밀번호", example="4321"),
+        'phone': fields.String(description="전화번호", example="02-0000-0000"),
+        'address': fields.String(description="주소", example="서울특별시 성북구 화랑도 11길 26"),
+        'registration_num': fields.String(description="사업자 등록 번호", example="114-54-235642"),
+        'file': fields.String(description="사업자 등록증", example="사업자등록증.pdf"),
+    })
+
+    delete_fields = auth_api.model("회원 가입 시 필요한 데이터", {
+        'user': fields.String(description="아이디", example="cwy"),
+    })
+
+
+@auth_api.route('/')
 @auth_api.expect(_Schema.post_fields)
 class signUp(Resource):
     ## 개인 회원 회원가입
@@ -69,8 +88,8 @@ class signUp(Resource):
             body = '{ "Success": false }'
             return Response(response=json.dumps(body), status=500, mimetype="application/json")
 
-@auth_api.route('/company/auth')
-@auth_api.expect(_Schema.post_fields)
+@auth_api.route('/company')
+@auth_api.expect(_Schema.post_fields_2)
 class signUp(Resource):
     ## 기업 회원 회원가입
     def post(self):
@@ -106,7 +125,7 @@ class signUp(Resource):
             body = '{ "Success": false }'
             return Response(response=json.dumps(body), status=500, mimetype="application/json")
 
-@auth_api.route('/admin/auth')
+@auth_api.route('/admin')
 @auth_api.expect(_Schema.post_fields)
 class adminSignUp(Resource):
     def post(self):
@@ -132,8 +151,8 @@ class adminSignUp(Resource):
             body = '{ "Success": false }'
             return Response(response=json.dumps(body), status=500, mimetype="application/json")
         
-@auth_api.route('/group')
-@auth_api.expect(_Schema.post_fields)
+@group_api.route('/')
+@group_api.expect(_Schema.post_fields)
 class group(Resource):
     def patch(self):
         req = request.args
@@ -172,19 +191,6 @@ class group(Resource):
         else:
             body = '{ "Success": false }'
             return Response(response=json.dumps(body), status=500, mimetype="application/json")
-
-@auth_api.route('/projectId')
-class addProjectId(Resource):
-    def post(self):
-        req = request.args
-        project_name = req.get('name')
-
-        res = ks_project.post_project_id(project_name)
-
-        if int(str(res)[11:14]) <= 204:
-            return "{ Success: true }"
-        else:
-            return "{ Success: False }"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='3000')
